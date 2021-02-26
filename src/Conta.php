cd3b9b77405bcd55843e1730e24fe0762e3acf87
Vini Black <viniibl4ck@gmail.com>
2021-02-26 17:50:03 -0300

    melhorando o codigo

--- src/Conta.php
+++ src/Conta.php
@@ -2,24 +2,62 @@
 
 class Conta
 {
-  public string $cpfTitular;
-  public string $nomeTitular;
-  public float $saldo = 0;
+  private $cpfTitular;
+  private $nomeTitular;
+  private $saldo = 0;
 
-  public function sacar(float $valorASacar)
+  public function saca(float $valorASacar): void
   {
     if ($valorASacar > $this->saldo) {
       echo "Saldo indisponível";
-    } else {
-      $this->saldo -= $valorASacar;
+      return;
     }
+
+    $this->saldo -= $valorASacar;
   }
-  public function depositar(float $valorADepositar): void
+
+  public function deposita(float $valorADepositar): void
   {
     if ($valorADepositar < 0) {
-      echo "Não é possivel depósitar $valorADepositar";
-    } else {
-      $this->saldo += $valorADepositar;
+      echo "Valor precisa ser positivo";
+      return;
     }
+
+    $this->saldo += $valorADepositar;
+  }
+
+  public function transfere(float $valorATransferir, Conta $contaDestino): void
+  {
+    if ($valorATransferir > $this->saldo) {
+      echo "Saldo indisponível";
+      return;
+    }
+
+    $this->saca($valorATransferir);
+    $contaDestino->deposita($valorATransferir);
+  }
+
+  public function recuperaSaldo(): float
+  {
+    return $this->saldo;
+  }
+
+  public function defineCpfTitular(string $cpf): void
+  {
+    $this->cpfTitular = $cpf;
+  }
+
+  public function recuperaCpfTitular(): string
+  {
+    return $this->cpfTitular;
+  }
+
+  public function defineNomeTitular(string $nome): void
+  {
+    $this->nomeTitular = $nome;
+  }
+  public function recuperarNomeTitular(): string
+  {
+    return $this->nomeTitular;
   }
 }
