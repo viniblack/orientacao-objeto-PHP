bdc22d82cd9faf085aea73e33a4f6b665d8b0d33
Vini Black <viniibl4ck@gmail.com>
2021-03-01 15:18:13 -0300

    entendendo composiçao de objetos

--- banco.php
+++ banco.php
@@ -1,16 +1,19 @@
 <?php
 
 require_once 'src/Conta.php';
+require_once 'src/Titular.php';
 
-$primeiraConta = new Conta('123.456.789-10', 'Vinicius');
+$primeiraConta = new Conta(new Titular('123.456.789-10', 'Vinicius Dias'));
+
 $primeiraConta->deposita(500);
-$primeiraConta->saca(300);
+$primeiraConta->saca(300); // isso é ok
 
-echo $primeiraConta->recuperarNomeTitular() . PHP_EOL;
+echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
 echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
 echo $primeiraConta->recuperaSaldo() . PHP_EOL;
 
-$segundaConta = new Conta('698.549.548-10', 'Patricia');
+$segundaConta = new Conta(new Titular('698.549.548-10', 'Patricia'));
+var_dump($segundaConta);
 
-new Conta('123',"dasdasdas");
+$outra = new Conta(new Titular('123', 'Abcdefg'));
 echo Conta::recuperaNumeroDeContas();
