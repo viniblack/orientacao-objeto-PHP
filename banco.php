9f4a673f237c5f714cc6ed5d2ee1b0597de39b18
Vini Black <viniibl4ck@gmail.com>
2021-03-01 15:28:22 -0300

    fazendo uma pequena validaçao de CPF

--- banco.php
+++ banco.php
@@ -2,9 +2,10 @@
 
 require_once 'src/Conta.php';
 require_once 'src/Titular.php';
+require_once 'src/CPF.php';
 
-$primeiraConta = new Conta(new Titular('123.456.789-10', 'Vinicius Dias'));
-
+$vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias');
+$primeiraConta = new Conta($vinicius);
 $primeiraConta->deposita(500);
 $primeiraConta->saca(300); // isso é ok
 
@@ -12,8 +13,10 @@ echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
 echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
 echo $primeiraConta->recuperaSaldo() . PHP_EOL;
 
-$segundaConta = new Conta(new Titular('698.549.548-10', 'Patricia'));
+$patricia = new Titular(new CPF('698.549.548-10'), 'Patricia');
+$segundaConta = new Conta($patricia);
 var_dump($segundaConta);
 
-$outra = new Conta(new Titular('123', 'Abcdefg'));
+$outra = new Conta(new Titular(new CPF('123.567.123-59'), 'Abcdefg'));
+unset($segundaConta);
 echo Conta::recuperaNumeroDeContas();
--- src/Titular.php
+++ src/Titular.php
@@ -2,30 +2,31 @@
 
 class Titular
 {
-  private $cpf;
-  private $nome;
+    private $cpf;
+    private $nome;
 
-  public function __construct(string $cpf, string $nome)
-  {
-    $this->cpf = $cpf;
-    $this->validaNomeTitular($nome);
-    $this->nome = $nome;
-  }
-
-  private function validaNomeTitular(string $nomeTitular)
-  {
-    if (strlen($nomeTitular) < 5) {
-      echo "Nome precisa ter pelo menos 5 caracteres";
-      exit();
+    public function __construct(CPF $cpf, string $nome)
+    {
+        $this->cpf = $cpf;
+        $this->validaNomeTitular($nome);
+        $this->nome = $nome;
     }
-  }
-  public function recuperaCpf(): string
-  {
-    return $this->cpf;
-  }
 
-  public function recuperaNome(): string
-  {
-    return $this->nome;
-  }
+    private function validaNomeTitular(string $nomeTitular)
+    {
+        if (strlen($nomeTitular) < 5) {
+            echo "Nome precisa ter pelo menos 5 caracteres";
+            exit();
+        }
+    }
+    public function recuperaCpf(): string
+    {
+        return $this->cpf->recuperaNumero();
+    }
+
+    public function recuperaNome(): string
+    {
+        return $this->nome;
+    }
+
 }
