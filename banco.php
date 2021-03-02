38f55c1b09d0eca5931abc25e6b2a79d2446b639
Vini Black <viniibl4ck@gmail.com>
2021-03-02 14:54:49 -0300

    aprendendo sobre herança, parent e protected

--- banco.php
+++ banco.php
@@ -1,10 +1,12 @@
 <?php
 
 require_once 'src/Conta.php';
+require_once 'src/Endereco.php';
 require_once 'src/Titular.php';
 require_once 'src/CPF.php';
 
-$vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias');
+$endereco = new Endereco('Petrópolis', 'um bairro', 'minha rua', '71B');
+$vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias', $endereco);
 $primeiraConta = new Conta($vinicius);
 $primeiraConta->deposita(500);
 $primeiraConta->saca(300); // isso é ok
@@ -13,10 +15,11 @@ echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
 echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
 echo $primeiraConta->recuperaSaldo() . PHP_EOL;
 
-$patricia = new Titular(new CPF('698.549.548-10'), 'Patricia');
+$patricia = new Titular(new CPF('698.549.548-10'), 'Patricia', $endereco);
 $segundaConta = new Conta($patricia);
 var_dump($segundaConta);
 
-$outra = new Conta(new Titular(new CPF('123.567.123-59'), 'Abcdefg'));
+$outroEndereco = new Endereco('A', 'B', 'C', '1D');
+$outra = new Conta(new Titular(new CPF('123.567.123-59'), 'Abcdefg', $outroEndereco));
 unset($segundaConta);
 echo Conta::recuperaNumeroDeContas();
--- src/Titular.php
+++ src/Titular.php
@@ -1,32 +1,20 @@
 <?php
 
-class Titular
+class Titular extends Pessoa
 {
-    private $cpf;
-    private $nome;
 
-    public function __construct(CPF $cpf, string $nome)
+    private $endereco;
+
+    public function __construct(CPF $cpf, string $nome, Endereco $endereco)
     {
-        $this->cpf = $cpf;
-        $this->validaNomeTitular($nome);
-        $this->nome = $nome;
+        parent::___construct($nome, $cpf); 
+        $this->endereco = $endereco;
     }
 
-    private function validaNomeTitular(string $nomeTitular)
-    {
-        if (strlen($nomeTitular) < 5) {
-            echo "Nome precisa ter pelo menos 5 caracteres";
-            exit();
-        }
-    }
-    public function recuperaCpf(): string
-    {
-        return $this->cpf->recuperaNumero();
-    }
 
-    public function recuperaNome(): string
-    {
-        return $this->nome;
-    }
 
+    public function recuperaEndereco(): Endereco
+    {
+        return $this->endereco;
+    }
 }
